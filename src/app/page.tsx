8cc4ba66b8ac261615269b59b9ea512829b549de
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-07-27 09:40:18 -0300

    feat: render EM DESENVOLVIMENTO button in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@
 import { Oxanium } from 'next/font/google';
 import { useEffect, useState } from 'react';
 
-const oxanium = Oxanium({ subsets: ['latin'], weight: '800' });
+import { Button } from './components';
 
+const oxanium = Oxanium({ subsets: ['latin'], weight: '800' });
 
 export default function Home() {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
@@ -27,12 +28,12 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div
-        style={parallaxStyle}
-        className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#ffffff] after:dark:opacity-40 before:lg:h-[360px]"
-      >
-        <p className="text-5xl">help or</p>
-        <p className={`text-5xl ${oxanium.className}`}>&nbsp;&nbsp;GTFO</p>
+      <div style={parallaxStyle} className="flex flex-col gap-6">
+        <div className="flex flex-row">
+          <p className="text-5xl">help or</p>
+          <p className={`text-5xl ${oxanium.className}`}>&nbsp;&nbsp;GTFO</p>
+        </div>
+        <Button color="red">Em desenvolvimento</Button>
       </div>
     </main>
   );
