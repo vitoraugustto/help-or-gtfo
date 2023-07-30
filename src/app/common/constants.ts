1e04151999e0deebc1eef6117fad64e8c22a4db5
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-07-29 23:39:50 -0300

    chore: create constants.ts

---
+++ src/app/common/constants.ts
@@ -0,0 +1,3 @@
+export const GITHUB_LINK = 'https://github.com/vitoraugustto';
+export const STEAM_LINK = 'https://steamcommunity.com/id/Vitor_Augustto';
+export const DISCORD_LINK = 'https://discordapp.com/users/259487137672462346';
