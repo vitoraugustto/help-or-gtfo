3b4ce22004b7ad4d5e70a138937df3afc3d65b0b
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-07-30 00:04:21 -0300

    refactor: slide button with span tag

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -16,10 +16,9 @@ export function Button(props: {
       className={`${handleColor(color)} 
       ${className} 
       ${fullWidth && 'min-w-full'}
-      min-h-[48px]
-      w-max max-w-[240px] border-b-2 border-l-8 border-r-2 border-t-2 bg-black bg-opacity-30 px-6 py-2 uppercase duration-200 ease-in max-sm:px-2`}
+      slide-button relative min-h-[48px] w-max max-w-[240px] border-b-2 border-l-8 border-r-2 border-t-2 bg-black bg-opacity-30 px-6 py-2 uppercase duration-200 ease-in max-sm:px-2`}
     >
-      {children}
+      <span>{children}</span>
     </button>
   );
 }
@@ -27,13 +26,13 @@ export function Button(props: {
 function handleColor(color: ButtonColors) {
   switch (color) {
     case 'red':
-      return 'border-[red] text-[red] button-hover-red';
+      return 'border-[red] text-[red] slide-button-red';
 
     case 'yellow':
-      return 'border-yellow-500 text-yellow-500 button-hover-yellow';
+      return 'border-yellow-500 text-yellow-500 slide-button-yellow';
 
     default:
-      return 'border-slate-200 text-slate-200 button-hover-slate';
+      return 'border-slate-200 text-slate-200 slide-button-slate';
   }
 }
 
