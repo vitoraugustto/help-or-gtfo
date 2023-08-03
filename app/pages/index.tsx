669584db9c10f4d986729a361131f988d00cc7b3
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-02 23:51:31 -0300

    feat: delete src folder

---
+++ app/pages/index.tsx
@@ -0,0 +1,5 @@
+import RootLayout from '../layout';
+
+export default function Home() {
+  return <RootLayout>Home</RootLayout>;
+}
--- src/app/pages/home/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <ul>
-        <li>:// RUNDOWNS</li>
-      </ul>
-    </main>
-  );
-}
