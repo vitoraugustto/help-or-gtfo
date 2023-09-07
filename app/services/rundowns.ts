5328c3ff3f2e46dddae810c5e67302226dc31a07
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-09-06 23:41:05 -0300

    feat: create fetchExpeditions service

--- app/services/rundowns.ts
+++ app/services/rundowns.ts
@@ -1,6 +1,6 @@
 import { AxiosResponse } from 'axios';
 
-import { IRundown } from '../common/types';
+import { IExpedition, IRundown } from '../common/types';
 import { instance } from './axios';
 
 export const fetchRundowns = (): Promise<
@@ -10,3 +10,11 @@ export const fetchRundowns = (): Promise<
     payload: IRundown[];
   }>
 > => instance({ url: '/api/v1/rundowns' });
+
+export const fetchExpeditions = (): Promise<
+  AxiosResponse<{
+    status: 'error' | 'success';
+    message: string;
+    payload: IExpedition[];
+  }>
+> => instance({ url: '/api/v1/expediotions' });
