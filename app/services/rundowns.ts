96dc9628482fbc6d30d2db3806d516a09e8f5f14
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-09-11 23:01:33 -0300

    feat: create fetchRundowns and fetchExpedition with fetchApi

--- app/services/rundowns.ts
+++ app/services/rundowns.ts
@@ -1,26 +1,27 @@
-import { AxiosResponse } from 'axios';
-
 import { IExpedition, IRundown } from '../common/types';
-import { instance } from './axios';
+import { fetchApi } from './fetch';
 
-export const fetchRundowns = (): Promise<
-  AxiosResponse<{
-    status: 'error' | 'success';
-    message: string;
-    payload: IRundown[];
-  }>
-> => instance({ url: '/api/v1/rundowns' });
+export const fetchRundowns = async (): Promise<{
+  status: 'error' | 'success';
+  message: string;
+  payload: IRundown[];
+}> => {
+  const res = await fetchApi('/api/v1/rundowns');
 
-export const fetchExpedition = (
+  return await res.json();
+};
+
+export const fetchExpedition = async (
   rundownId: IRundown['id'],
   expeditionId: IExpedition['id'],
-): Promise<
-  AxiosResponse<{
-    status: 'error' | 'success';
-    message: string;
-    payload: IExpedition;
-  }>
-> =>
-  instance({
-    url: `/api/v1/rundowns/${rundownId}/expeditions/${expeditionId}`,
-  });
+): Promise<{
+  status: 'error' | 'success';
+  message: string;
+  payload: IExpedition;
+}> => {
+  const res = await fetchApi(
+    `/api/v1/rundowns/${rundownId}/expeditions/${expeditionId}`,
+  );
+
+  return await res.json();
+};
