9531066925d2dca812ddf0ef3f774465dc9eb6af
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-09-12 23:51:23 -0300

    feat: remove axios.ts

--- app/services/axios.ts
+++
@@ -1,6 +0,0 @@
-import axios, { AxiosInstance } from 'axios';
-
-export const instance: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
-  headers: { 'Content-Type': 'application/json' },
-});
