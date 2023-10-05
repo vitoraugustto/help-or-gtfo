c65e674833c8d86265dc86bd6c41c26a5858e385
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-10-05 00:59:45 -0300

    feat: add SECTORS in Expedition page

--- app/complex/(parallax)/rundowns/[rundownId]/expeditions/[expeditionId]/page.tsx
+++ app/complex/(parallax)/rundowns/[rundownId]/expeditions/[expeditionId]/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { IExpedition, IRundown } from '@/app/common/types';
 import { fetchExpedition } from '@/app/services/rundowns';
 
@@ -10,6 +12,24 @@ async function getExpedition(
   return payload;
 }
 
+const SECTORS = [
+  {
+    src: '/images/sector-main.webp',
+    title: 'MAIN',
+    alt: 'Sector Main',
+  },
+  {
+    src: '/images/sector-secondary.webp',
+    title: 'SECONDARY',
+    alt: 'Sector Secondary',
+  },
+  {
+    src: '/images/sector-overload.webp',
+    title: 'OVERLOAD',
+    alt: 'Sector Overload',
+  },
+];
+
 export default async function Expedition({
   params,
 }: {
@@ -19,16 +39,35 @@ export default async function Expedition({
   const expedition = await getExpedition(rundownId, expeditionId);
 
   return (
-    <div>
-      <p className="text-2xl">{expedition.display_name}</p>
-      <p>Quantidade de XP por concluir: {expedition.xp}</p>
-      <p>Prisioneiros que finalizaram a expedição:</p>
-      {expedition.finishers?.map((prisoner) => (
-        <div className="flex flex-row">
-          <p>Level {prisoner.level} -&nbsp;</p>
-          <p>{prisoner.username}</p>
+    <div className="flex w-full flex-col">
+      <div className="flex flex-col gap-4">
+        <p className="text-4xl">
+          {expedition.display_name} - {expedition.title}
+        </p>
+        <div className="flex flex-row gap-4">
+          {SECTORS.map((sector) => (
+            <div className="flex h-16 w-16 items-center">
+              <Image
+                src={sector.src}
+                alt={sector.alt}
+                width={0}
+                height={0}
+                sizes="100vw"
+                className="h-auto w-full"
+              />
+            </div>
+          ))}
         </div>
-      ))}
+
+        <p>Quantidade de XP por concluir: {expedition.xp}</p>
+        <p>Prisioneiros que finalizaram a expedição:</p>
+        {expedition.finishers?.map((prisoner) => (
+          <div className="flex flex-row border-2 border-yellow-500 p-2 text-yellow-500">
+            <p>Level {prisoner.level} -&nbsp;</p>
+            <p>{prisoner.username}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
