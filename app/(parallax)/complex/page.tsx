2daac80f9df200493fc62b4f43a8c44dc76fab9d
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-10-11 23:52:31 -0300

    style: rename screen from Complex to ComplexScreen

--- app/(parallax)/complex/page.tsx
+++ app/(parallax)/complex/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 import { Button } from '@/app/components';
 
-export default function Complex() {
+export default function ComplexScreen() {
   return (
     <div className="h- flex h-screen w-full items-center justify-center">
       <div className="flex flex-col gap-4">
