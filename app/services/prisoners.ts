8afa2e8e52442248159402fccbcd2a8d838d7a83
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-10-16 22:59:45 -0300

    feat: create fetchPrisonerCompletedExpeditionsservice

--- app/services/prisoners.ts
+++ app/services/prisoners.ts
@@ -1,4 +1,4 @@
-import { IPrisoner } from '../common/types';
+import { ICompletedExpeditions, IExpedition, IPrisoner } from '../common/types';
 import { fetchApi } from './fetch';
 
 export const fetchPrisoners = async (): Promise<{
@@ -7,6 +7,30 @@ export const fetchPrisoners = async (): Promise<{
   payload: IPrisoner[];
 }> => {
   const res = await fetchApi('/api/v1/prisoners?order_by=xp&sort_order=desc');
+
+  return await res.json();
+};
+
+export const fetchPrisoner = async (
+  id: IPrisoner['id'],
+): Promise<{
+  status: 'error' | 'success';
+  message: string;
+  payload: IPrisoner;
+}> => {
+  const res = await fetchApi(`/api/v1/prisoners/${id}`);
+
+  return await res.json();
+};
+
+export const fetchPrisonerCompletedExpeditions = async (
+  id: IPrisoner['id'],
+): Promise<{
+  status: 'error' | 'success';
+  message: string;
+  payload: ICompletedExpeditions[];
+}> => {
+  const res = await fetchApi(`/api/v1/prisoners/${id}/completed-expeditions`);
 
   return await res.json();
 };
