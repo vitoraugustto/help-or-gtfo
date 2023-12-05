6a44b6a46385cb285bb26ba3c32436dcd8fded0d
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-12-04 23:54:53 -0300

    feat: add types in fetchPrisoners

--- app/services/prisoners.ts
+++ app/services/prisoners.ts
@@ -8,11 +8,9 @@ import {
 } from '../common/types';
 import { fetchApi } from './fetch';
 
-export const fetchPrisoners = async (): Promise<{
-  status: ResponseStatus;
-  message: string;
-  payload: IPrisoner[];
-}> => {
+export const fetchPrisoners = async (): Promise<
+  IBackendResponse & { payload: IPrisoner[] }
+> => {
   const res = await fetchApi('/api/v1/prisoners?order_by=xp&sort_order=desc');
 
   return await res.json();
