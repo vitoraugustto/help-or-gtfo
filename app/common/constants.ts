3b441983640c24602ed8ef81be656051f8ff1d7f
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-12-14 23:54:13 -0300

    feat: create constant BACKEND_BASE_URL

--- app/common/constants.ts
+++ app/common/constants.ts
@@ -1,3 +1,5 @@
 export const GITHUB_LINK = 'https://github.com/vitoraugustto';
 export const STEAM_LINK = 'https://steamcommunity.com/id/Vitor_Augustto';
 export const DISCORD_LINK = 'https://discordapp.com/users/259487137672462346';
+
+export const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL
