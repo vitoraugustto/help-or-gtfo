16989bdb8a175af28c0c90edbc5a4eaf79b558fa
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-12-14 23:55:04 -0300

    feat: add BACKEND_BASE_URL in fetchApi

--- app/services/fetch.ts
+++ app/services/fetch.ts
@@ -1,8 +1,10 @@
+import { BACKEND_BASE_URL } from '../common/constants';
+
 export const fetchApi = async (url: string, options?: RequestInit) => {
   const opt: RequestInit = {
     ...options,
     headers: { 'Content-Type': 'application/json' },
   };
 
-  return await fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}${url}`, opt);
+  return await fetch(`${BACKEND_BASE_URL}${url}`, opt);
 };
