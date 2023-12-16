6627c55eec2df2e796e9c43030fdf122b4ae8c95
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-12-15 22:45:50 -0300

    feat: create headers constant in fetch.ts

--- app/services/fetch.ts
+++ app/services/fetch.ts
@@ -1,10 +1,9 @@
 import { BACKEND_BASE_URL } from '../common/constants';
 
+const headers = { 'Content-Type': 'application/json' };
+
 export const fetchApi = async (url: string, options?: RequestInit) => {
-  const opt: RequestInit = {
-    ...options,
-    headers: { 'Content-Type': 'application/json' },
-  };
+  const opt: RequestInit = { ...options, headers };
 
   return await fetch(`${BACKEND_BASE_URL}${url}`, opt);
 };
